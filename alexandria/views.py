81ed33a5ed69cd476565c15ac65e77bf7a765547
Carlos Barranco <barrancocarlos@hotmail.com>
2022-10-01 21:45:45 -0300

    single doc

--- alexandria/views.py
+++ alexandria/views.py
@@ -10,3 +10,9 @@ def nlp(request):
 	query_docs = Document.objects.all()
 	context = {"query_docs": query_docs}
 	return render(request, "nlp.html", context)
+
+def nlp_single(request):
+	doc_id = request.GET['id']
+	query_docs = Document.objects.get(pk=doc_id)
+	context = {"query_docs": query_docs}
+	return render(request, "nlp.html", context)
