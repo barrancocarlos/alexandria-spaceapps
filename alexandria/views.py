792cda24a4b40fa7a6bdf31a52416cc050700b3d
Carlos Barranco <barrancocarlos@hotmail.com>
2022-10-02 12:44:57 -0300

    nlp single css

--- alexandria/views.py
+++ alexandria/views.py
@@ -35,61 +35,61 @@ def nlpsingle(request):
     return render(request, "nlp-single.html", context)
 
 
-# def nlpsingle(request):
-#     doc_id = request.GET['id']
-#     query_docs = Document.objects.get(pk=doc_id)
-#     #Pdf path
-#     pre_url = "static/assets/corpus/"
-#     pos_url = ".pdf"
-#     pdf = query_docs.doc_id
-#     pdf_url = pre_url + pdf + pos_url
-# 	#NLP Summary
-#     mypdf = open(pdf_url, mode='rb')
-#     pdfReader = PyPDF2.PdfFileReader(mypdf)
-#     text = ''
-#     for i in range(0, pdfReader.numPages):
-#          pageObj = pdfReader.getPage(i)
-#          ext = text+pageObj.extractText()     
-# 	#tokenize
-#     stopWords = set(stopwords.words("english"))
-#     words = word_tokenize(text)
-# 	#frequency table
-#     freqTable = dict()
-#     for word in words:
-#         word = word.lower()
-#         if word in stopWords:
-#             continue
-#         if word in freqTable:
-#             freqTable[word] += 1
-#         else:
-#             freqTable[word] = 1    
-# 	#store sentences
-#     sentences = sent_tokenize(text)
-#     sentenceValue = dict()
-#     for sentence in sentences:
-#         for word, freq in freqTable.items():
-#             if word in sentence.lower():
-#                 if sentence in sentenceValue:
-#                     sentenceValue[sentence] += 1
-#                 else:
-#                     sentenceValue[sentence] = 1
+def nlprocess(request):
+    doc_id = request.GET['id']
+    query_docs = Document.objects.get(pk=doc_id)
+    #Pdf path
+    pre_url = "static/assets/corpus/"
+    pos_url = ".pdf"
+    pdf = query_docs.doc_id
+    pdf_url = pre_url + pdf + pos_url
+	#NLP Summary
+    mypdf = open(pdf_url, mode='rb')
+    pdfReader = PyPDF2.PdfFileReader(mypdf)
+    text = ''
+    for i in range(0, pdfReader.numPages):
+         pageObj = pdfReader.getPage(i)
+         ext = text+pageObj.extractText()     
+	#tokenize
+    stopWords = set(stopwords.words("english"))
+    words = word_tokenize(text)
+	#frequency table
+    freqTable = dict()
+    for word in words:
+        word = word.lower()
+        if word in stopWords:
+            continue
+        if word in freqTable:
+            freqTable[word] += 1
+        else:
+            freqTable[word] = 1    
+	#store sentences
+    sentences = sent_tokenize(text)
+    sentenceValue = dict()
+    for sentence in sentences:
+        for word, freq in freqTable.items():
+            if word in sentence.lower():
+                if sentence in sentenceValue:
+                    sentenceValue[sentence] += 1
+                else:
+                    sentenceValue[sentence] = 1
           
-#     sumValues = 0
-#     for sentence in sentenceValue:
-#         sumValues += sentenceValue[sentence]
-#         average = int(sumValues / len(sentenceValue))
-# 	#store sentences in summary
-#     summary = ''
-#     for sentence in sentences:
-#         if (sentence in sentenceValue) and (sentenceValue[sentence] > (2 * average)):
-#             summary += " " + sentence    
-#     rake_nltk_var = Rake()
-#     #remove punctuation
-#     text = text.translate(str.maketrans('', '', string.punctuation))
-#     #rake
-#     rake_nltk_var.extract_keywords_from_text(text)
-#     keyword_extracted = rake_nltk_var.get_ranked_phrases()
-#     #Creating FreqDist for whole BoW, keeping the 20 most common tokens
-#     all_fdist = nltk.FreqDist(keyword_extracted).most_common(20)       
-#     context = {"query_docs": query_docs, "summary": summary,"all_fdist": all_fdist}
-#     return render(request, "nlp-single.html", context)
+    sumValues = 0
+    for sentence in sentenceValue:
+        sumValues += sentenceValue[sentence]
+        average = int(sumValues / len(sentenceValue))
+	#store sentences in summary
+    summary = ''
+    for sentence in sentences:
+        if (sentence in sentenceValue) and (sentenceValue[sentence] > (2 * average)):
+            summary += " " + sentence    
+    rake_nltk_var = Rake()
+    #remove punctuation
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    #rake
+    rake_nltk_var.extract_keywords_from_text(text)
+    keyword_extracted = rake_nltk_var.get_ranked_phrases()
+    #Creating FreqDist for whole BoW, keeping the 20 most common tokens
+    all_fdist = nltk.FreqDist(keyword_extracted).most_common(20)       
+    context = {"query_docs": query_docs, "summary": summary,"all_fdist": all_fdist}
+    return render(request, "nlp-single.html", context)
