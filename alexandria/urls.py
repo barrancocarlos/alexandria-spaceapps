8f9608323cd11fc6f618bdd7a6d5b5544e5104cc
Carlos Barranco <barrancocarlos@hotmail.com>
2022-10-02 01:01:38 -0300

    open pdf

--- alexandria/urls.py
+++ alexandria/urls.py
@@ -3,5 +3,6 @@ from alexandria import views
 
 urlpatterns = [    
     path('', views.home, name='home'),
-    path('nlp/', views.nlp, name='nlp'),    
+    path('nlp/', views.nlp, name='nlp'),
+    path('nlp-single/', views.nlpsingle, name='nlpsingle'),     
 ]
--- alexandria/views.py
+++ alexandria/views.py
@@ -1,18 +1,33 @@
 from django.shortcuts import render
 from .models import Document
+import PyPDF2 as PyPDF2
 
 # Create your views here.
 
+
 def home(request):
 	return render(request, "index.html", {})
+
 
 def nlp(request):
 	query_docs = Document.objects.all()
 	context = {"query_docs": query_docs}
 	return render(request, "nlp.html", context)
 
-def nlp_single(request):
+
+def nlpsingle(request):
 	doc_id = request.GET['id']
 	query_docs = Document.objects.get(pk=doc_id)
-	context = {"query_docs": query_docs}
-	return render(request, "nlp.html", context)
+	#NL Processing
+	pre_url = "static/assets/corpus/"
+	pos_url = ".pdf"
+	pdf = query_docs.doc_id
+	pdf_url = pre_url + pdf + pos_url
+	mypdf = open(pdf_url, mode='rb')
+	pdfReader = PyPDF2.PdfFileReader(mypdf)
+	text = ''
+	for i in range(0, pdfReader.numPages):    
+		pageObj = pdfReader.getPage(i)
+		text=text+pageObj.extractText()
+	context = {"query_docs": query_docs, "text": text}
+	return render(request, "nlp-single.html", context)
